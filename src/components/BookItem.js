77554c0d1664fae29926a29f9338a50766a0072b
“doniwirawan” <doniwirawan166@gmail.com>
2021-09-26 10:05:16 +0800

    improve

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -6,19 +6,18 @@ const BookItem = ({ nama, img, link, desc, tags }) => {
             <div className="col-lg-4  col-md-12 p-3 border-top border-bottom">
 
                 <div className="col">
-                    <img src={img} alt="test" className="img-fluid rouded" />
+                    <img src={img} alt="test" className="img-fluid rounded book-img" />
                 </div>
                 <div className="col pl-2 mt-1">
-                    <h2 className="h4 gradient-text font-weight-medium text-capitalize">{nama}</h2>
-                    {tags.map((item, index) => <small className="tag mx-1" key={index}>{item}</small>)}
+                    <h2 className="h4 gradient-text font-weight-medium text-capitalize ml-2">{nama}</h2>
+                    {tags.map((item, index) => <small className="tag mx-1 p
+                    ml-2" key={index}>{item}</small>)}
                 </div>
 
                 <p className="text-secondary mt-1 project-description pl-2">{desc}</p>
-                <div className="row ml-4">
-                    <div className="row ">
-                        <a href={link} target="_blank" className="btn btn-gradient"><i
-                            className="fa fa-globe"></i> Link</a>
-                    </div>
+                <div className="row ml-3">
+                    <a href={link} target="_blank" className="btn btn-gradient"><i
+                        className="fa fa-globe"></i> Link</a>
 
                 </div>
             </div>
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const ProjectItem = ({nama, img, link, desc, tags}) => {
+const ProjectItem = ({ nama, img, link, desc, tags }) => {
     return (
         <>
             <div className="col-lg-4  col-md-12 p-3 border-top border-bottom">
-                        <div className="row">
-                            <div className="col">
-                                <img src={img} alt="test" className="img-fluid rouded"/>
-                            </div>
-                            <div className="col pl-2">
-                                <h2 className="h4 gradient-text font-weight-medium">{nama}</h2>
-                        {tags.map((item, index) => <small className="tag mx-1" key={index}>{item}</small> )}
-                                
-                                
-                            </div>
-                        </div>
-                        <p className="text-secondary mt-1 project-description">{desc}</p>
-                        <div className="row ml-3">
-                            <div className="row ">
+                <div className="row">
+                    <div className="col">
+                        <img src={img} alt="test" className="img-fluid rounded" />
+                    </div>
+                    <div className="col pl-2">
+                        <h2 className="h4 gradient-text font-weight-medium">{nama}</h2>
+                        {tags.map((item, index) => <small className="tag mx-1" key={index}>{item}</small>)}
+
+
+                    </div>
+                </div>
+                <p className="text-secondary mt-1 project-description">{desc}</p>
+                <div className="row ml-3">
+                    <div className="row ">
                         <a href={link[1]} target="_blank" className="btn btn-gradient"><i
-                                        className="fa fa-globe"></i> Demo</a>
-                            </div>
-                            <div className="row ml-4">
-                                <a href={link[0]} target="_blank" className="btn btn-secondary"><i
-                                        className="fab fa-github"></i> Github</a>
-                            </div>
-                        </div>
-                    </div> 
+                            className="fa fa-globe"></i> Demo</a>
+                    </div>
+                    <div className="row ml-4">
+                        <a href={link[0]} target="_blank" className="btn btn-secondary"><i
+                            className="fab fa-github"></i> Github</a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
